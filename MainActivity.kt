1a50a650df273f60aa760aa0647d3756e7b96874
Alina <alinaroshchina5@gmail.com>
2026-02-26 09:43:21 +0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -24,44 +26,45 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.navigation.ui.theme.NavigationTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            NavHost(navController,"Home"){
-                composable(Screens.HOME.route) {
-                    Home()
-                }
-                composable("profile") {
-                    Profile()
-                }
-                composable(Screens1.About.route) {
-                    About()
+            Column {
+                NavBar(navController)
+                NavHost(navController, "home") {
+                    composable("home") {
+                        Home()
+                    }
+                    composable(Screens.PROFILE.route + "/{userId}",
+                        arguments = listOf(
+                            navArgument("userId") {
+                                type = NavType.IntType
+                        }
+                        )) { stackEntry ->
+
+                        val userId = stackEntry.arguments?.getInt("userId")
+
+                        Profile(userId)
+                    }
+                    composable(Screens1.About.route) {
+                        About()
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Home(){
-    Text("Главная")
-}
-
-@Composable
-fun Profile(){
-    Text("Профиль")
-}
-
-@Composable
-fun About(){
-    Text("О программе")
 }
 
 enum class Screens(val route: String){
@@ -72,6 +75,58 @@ enum class Screens(val route: String){
 
 sealed class Screens1(val route: String){
     object Home: Screens1("home")
-    object Profile: Screens1("profile")
+    object Profile: Screens1("profile/{userId}") {
+        fun createRoute(userId:Int) = "profile/${userId}"
+    }
     object About: Screens1("about")
+}
+
+@Composable
+fun NavBar(navController: NavController) {
+
+    Row(Modifier
+        .fillMaxWidth()
+        .padding(5.dp)) {
+        Text("Home",
+            Modifier
+                .weight(1f)
+                .clickable {
+                    navController.navigate(Screens.HOME.route)
+                },
+            fontSize = 25.sp
+        )
+        Text("Profile",
+            Modifier
+                .weight(1f)
+                .clickable {
+                    navController.navigate(
+                        Screens1.Profile.createRoute(Random.nextInt(100))
+                    )
+                },
+            fontSize = 25.sp
+        )
+        Text("About",
+            Modifier
+                .weight(1f)
+                .clickable {
+                    navController.navigate(Screens.ABOUT.route)
+                },
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Composable
+fun Home(){
+    Text("Главная", fontSize = 30.sp)
+}
+
+@Composable
+fun Profile(id: Int?){
+    Text("Профиль $id", fontSize = 30.sp)
+}
+
+@Composable
+fun About(){
+    Text("О программе", fontSize = 30.sp)
 }
